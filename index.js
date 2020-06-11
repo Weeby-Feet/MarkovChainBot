8b254092ed5c3927213928eeaf0d3f30c742714d
Nathan Ellison <nathan.ipadman@gmail.com>
2020-06-11 18:56:08 +1200

    Comments

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 
+//Split the message words up into an array
+
+//Step through the array
+
+//Determine if the current word is already in the bot's memory
+
+//If not, add it
+
 });
 
+//Function used for generating random numbers to determine which words to use
 function randInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
