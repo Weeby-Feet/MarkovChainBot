544836c4ae5b7a73f1e4e34f5ac47d4b967e8bb4
Nathan Ellison <nathan.ipadman@gmail.com>
2020-07-01 11:23:07 +1200

    2D array is now constructed without adding duplicate entries

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const bot = new Discord.Client();
 
 const token = require('./token.js').token;
 
-var words = [1][1];
+
+var chain = new Array();
+// for(i = 0; i < chain.length; i++) {
+//   chain[i] = new Array(10);
+// }
+
 
 bot.on('ready', () => {
   console.log('Bot is ready to do some Markoving');
+  //Control array entry to ensure code knows the chain array is 2 dimensional
+  chain.push(new Array("."));
 });
 
 bot.on('message', message => {
@@ -15,25 +22,40 @@ bot.on('message', message => {
   messageArr = message.content.split(" ");
 
   //Step through the array
-  for(i = 0; i < messageArr.size(); i++) {
+  for(i = 0; i < messageArr.length; i++) {
+
+    var contains = false;
 
     //Determine if the current word is already in the bot's memory
-    var contains = messageArr.includes(words[i]);
-
-    //If not, add it
-    if(!contains) {
-      //Add word
+    for(j = 0; j < chain.length; j++) {
+      if(chain[j] == messageArr[i]) {
+        contains = true;
+      }
     }
 
-    //Do other things
+    //If not, add it
+    if(contains == false) {
+      //Add word
+      chain.push(new Array(messageArr[i]));
+    }
 
   }
+
+  console.log(chain);
 
 });
 
 //Function used for generating random numbers to determine which words to use
 function randInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function expandArray(arr) {
+
+}
+
+function genText() {
+
 }
 
 bot.login(token);
