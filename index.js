de0956fb4acaf57b70838bd4b2036ce58455e939
Nathan Ellison <nathan.ipadman@gmail.com>
2020-07-05 12:55:20 +1200

    Chain generation moved to seperate function. Text generation function has also been implemented. Added skeleton code for learning from a text file

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const bot = new Discord.Client();
 
 const token = require('./token.js').token;
 
+const file = require('file-system');
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 
 var chain = new Array();
 // for(i = 0; i < chain.length; i++) {
@@ -12,8 +19,18 @@ var chain = new Array();
 
 bot.on('ready', () => {
   console.log('Bot is ready to do some Markoving');
-  //Dummy array entry to ensure code knows the chain array is 2 dimensional
+  //Dummy array entry to ensure code knows the chain array is 2 dimensional and avoid not defined errors
   chain.push(new Array(""));
+
+  //Ask the person running the bot if they would like to setup the bot's memory with a file
+  readline.question(`Would you like the bot to learn some words from a file first? (y/n) `, (input) => {
+    if(input == 'y') {
+      learnFromFile();
+      return;
+    }
+    console.log("Not learning from file");
+    readline.close();
+  })
 });
 
 bot.on('message', message => {
@@ -21,10 +38,26 @@ bot.on('message', message => {
   if(message.author.bot) return;
   if(message.content.toLowerCase().includes("mark")) genText(message.channel);
 
+  genChain(message.content);
+
+});
+
+//Function used for generating random numbers to determine which words to use
+function randInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//Function might not be needed
+function expandArray(arr) {
+
+}
+
+function genChain(data) {
+
   var index;
 
   //Split the message words up into an array
-  messageArr = message.content.split(" ");
+  messageArr = data.split(" ");
 
   //Step through the array
   for(i = 0; i < messageArr.length; i++) {
@@ -52,19 +85,21 @@ bot.on('message', message => {
   }
 
   console.log(chain);
-
-  //genText();
-
-});
-
-//Function used for generating random numbers to determine which words to use
-function randInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
 
-//Function might not be needed
-function expandArray(arr) {
+//Function for getting the bot to build a chain based on a file of sentences
+function learnFromFile() {
+  console.log("Opening file system...\n");
 
+  file.readFile('dict.txt', 'utf8', function(err, data) {
+    if(err) {
+      throw err;
+    }
+    else {
+      console.log("File read successfully");
+      genChain(data);
+    }
+  })
 }
 
 //Function for generating output using the chain constructed above
